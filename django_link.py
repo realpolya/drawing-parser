7fd670df67d277ae74784f220bbe6dbf4c27f6f5
realpolya <ps2780@columbia.edu>
2025-11-12 18:33:44 -0500

    django pdf parser

---
+++ django_link.py
@@ -0,0 +1,10 @@
+# import pymupdf
+# from django.core.files.storage import default_storage
+
+# from .models import MyModel
+
+# obj = MyModel.objects.get(id=1)
+# with default_storage.open(obj.file.name) as f:
+#     data = f.read()
+
+# doc = pymupdf.Document(stream=data)
