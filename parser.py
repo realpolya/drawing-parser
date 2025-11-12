8472833118904f1a0d2e922b7295c00768d54798
realpolya <ps2780@columbia.edu>
2025-11-11 21:15:16 -0500

    start simple with pdfminer library

--- parser.py
+++ parser.py
@@ -1,1 +1,9 @@
 # starting a drawing parser code
+
+import re
+from pdfminer.high_level import extract_pages, extract_text
+
+for page in extract_pages("./data/sample.pdf"):
+
+    for element in page:
+        print(element)
