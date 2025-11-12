31164befd6525f21e70caea7ffe615a85cc47f8a
realpolya <ps2780@columbia.edu>
2025-11-11 21:43:35 -0500

    create and save images via PyMuPDF

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@
 import re
 from pdfminer.high_level import extract_pages, extract_text
 
+'''Extract text with pdf miner'''
 # for page in extract_pages("./data/sample.pdf"):
 
 #     for element in page:
@@ -21,3 +22,23 @@ print(dim_matches)
 
 for m in dim_matches:
     print(m)
+
+
+'''Extract image PyMuPDF'''
+import fitz
+import PIL.Image
+import io
+
+pdf = fitz.open("./data/hyatt.pdf")
+count = 1
+for i in range(len(pdf)):
+    # iterate over the pages
+    page = pdf[i]
+    images = page.get_images()
+    for image in images:
+        base_img = pdf.extract_image(image[0])
+        image_data = base_img["image"]
+        img = PIL.Image.open(io.BytesIO(image_data))
+        extension = base_img["ext"]
+        img.save(open(f"image{count}.{extension}", "wb"))
+        count += 1
