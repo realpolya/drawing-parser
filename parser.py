98c1c0822ea56c870ca6a3b4814927ab5a5f6388
realpolya <ps2780@columbia.edu>
2025-11-12 18:12:49 -0500

    comment out unnecessary

--- parser.py
+++ parser.py
@@ -10,19 +10,19 @@ from pdfminer.high_level import extract_pages, extract_text
 #     for element in page:
 #         print(element)
 
-text = extract_text("./data/sample.pdf")
-# print(text)
+# text = extract_text("./data/sample.pdf")
+# # print(text)
 
-letter_pattern = re.compile(r"^[A-Z].*") # regex letter_pattern for an uppercase letter start of string
-matches = letter_pattern.findall(text)
-print(matches)
+# letter_pattern = re.compile(r"^[A-Z].*") # regex letter_pattern for an uppercase letter start of string
+# matches = letter_pattern.findall(text)
+# print(matches)
 
-dimension_pattern = re.compile(r"^\d+\'-\d+\"$", re.MULTILINE)
-dim_matches = dimension_pattern.findall(text)
-print(dim_matches)
+# dimension_pattern = re.compile(r"^\d+\'-\d+\"$", re.MULTILINE)
+# dim_matches = dimension_pattern.findall(text)
+# print(dim_matches)
 
-for m in dim_matches:
-    print(m)
+# for m in dim_matches:
+#     print(m)
 
 
 '''Extract image PyMuPDF'''
@@ -30,16 +30,17 @@ import fitz
 import PIL.Image
 import io
 
-pdf = fitz.open("./data/hyatt.pdf")
-count = 1
-for i in range(len(pdf)):
-    # iterate over the pages
-    page = pdf[i]
-    images = page.get_images()
-    for image in images:
-        base_img = pdf.extract_image(image[0])
-        image_data = base_img["image"]
-        img = PIL.Image.open(io.BytesIO(image_data))
-        extension = base_img["ext"]
-        img.save(open(f"image{count}.{extension}", "wb"))
-        count += 1
+# pdf = fitz.open("./data/hyatt.pdf")
+# count = 1
+# for i in range(len(pdf)):
+#     # iterate over the pages
+#     page = pdf[i]
+#     images = page.get_images()
+#     for image in images:
+#         base_img = pdf.extract_image(image[0])
+#         image_data = base_img["image"]
+#         img = PIL.Image.open(io.BytesIO(image_data))
+#         extension = base_img["ext"]
+#         img.save(open(f"image{count}.{extension}", "wb"))
+#         count += 1
+
