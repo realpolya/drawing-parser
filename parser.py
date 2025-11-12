beaa5d46777f42620b7820b35b8701f13480de18
realpolya <ps2780@columbia.edu>
2025-11-11 21:25:50 -0500

    update regex pattern

--- parser.py
+++ parser.py
@@ -3,7 +3,14 @@
 import re
 from pdfminer.high_level import extract_pages, extract_text
 
-for page in extract_pages("./data/sample.pdf"):
+# for page in extract_pages("./data/sample.pdf"):
 
-    for element in page:
-        print(element)
+#     for element in page:
+#         print(element)
+
+text = extract_text("./data/sample.pdf")
+# print(text)
+
+pattern = re.compile(r"^[A-Z].*") # regex pattern for an uppercase letter start of string
+matches = pattern.findall(text)
+print(matches)
