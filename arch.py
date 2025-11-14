5137ff018838b44c4bab15cd61ccbe140d0bb387
realpolya <ps2780@columbia.edu>
2025-11-13 20:10:02 -0500

    outline arch.py goals

---
+++ arch.py
@@ -0,0 +1,10 @@
+# testing parsing architecture pdf drawings
+# goals:
+# understand scale and dimensions
+# understand different line types (property line, dashed line, solid line, etc)
+# understand multiple lines forming a shape (rectangle, polygon, etc)
+# associate text with shapes
+# calculate areas of shape
+# calculate distances between shapes
+# understand drawing orientation (North)
+# test: can you calculate the distance between the property line and the proposed building? existing building?
